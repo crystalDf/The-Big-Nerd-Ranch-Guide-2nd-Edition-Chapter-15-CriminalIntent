6cbe0866c359e6fd8319f5ac27189f36b7fcfdcb
crystalDf <chendong333@gmail.com>
2017-01-07 01:07:45 +0800

    Upgrade to API Level 25

--- app/src/main/java/com/star/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/star/criminalintent/CrimeFragment.java
@@ -375,4 +375,5 @@ public class CrimeFragment extends Fragment {
         return getString(R.string.crime_report, mCrime.getTitle(), dateString,
                 solvedString, displayName);
     }
+
 }
